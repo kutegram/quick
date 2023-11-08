1193c1638a35e592aa7001be08ac4b33fb8fcdab
Mathew <me@crx.moe>
2023-11-08 15:46:14 +0300

    Windows tray icon

--- src/platformutils.cpp
+++ src/platformutils.cpp
@@ -1,5 +1,7 @@
 #include "platformutils.h"
 
+#include <QApplication>
+
 #if defined(Q_OS_WIN32) && QT_VERSION >= QT_VERSION_CHECK(5, 2, 0)
 #include <QtWinExtras/QtWin>
 #define DWM_FEATURES
@@ -8,8 +10,52 @@
 PlatformUtils::PlatformUtils(QWidget *parent)
     : QObject((QObject*) parent)
     , window(parent)
+    , trayIcon(this)
+    , trayMenu()
 {
+    window->setAttribute(Qt::WA_DeleteOnClose, false);
+    window->setAttribute(Qt::WA_QuitOnClose, false);
 
+    connect(&trayIcon, SIGNAL(messageClicked()), this, SLOT(messageClicked()));
+    connect(&trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)), this, SLOT(trayActivated(QSystemTrayIcon::ActivationReason)));
+    connect(&trayMenu, SIGNAL(triggered(QAction*)), this, SLOT(menuTriggered(QAction*)));
+
+    trayMenu.addAction("Open Kutegram");
+    trayMenu.addAction("Exit");
+
+    trayIcon.setContextMenu(&trayMenu);
+    trayIcon.setIcon(QIcon(":/kutegramquick_small.png"));
+    trayIcon.setToolTip("Kutegram");
+    trayIcon.show();
+}
+
+void PlatformUtils::showAndRaise()
+{
+    window->show();
+    window->activateWindow();
+    window->raise();
+}
+
+void PlatformUtils::trayActivated(QSystemTrayIcon::ActivationReason reason)
+{
+    if (reason != QSystemTrayIcon::Context) {
+        showAndRaise();
+    }
+}
+
+void PlatformUtils::messageClicked()
+{
+    showAndRaise();
+}
+
+void PlatformUtils::menuTriggered(QAction *action)
+{
+    if (action->text() == "Exit") {
+        QApplication::exit();
+        return;
+    }
+
+    showAndRaise();
 }
 
 void PlatformUtils::windowsExtendFrameIntoClientArea(int left, int top, int right, int bottom)
--- src/platformutils.h
+++ src/platformutils.h
@@ -3,12 +3,16 @@
 
 #include <QObject>
 #include <QColor>
+#include <QSystemTrayIcon>
+#include <QMenu>
 
 class PlatformUtils : public QObject
 {
     Q_OBJECT
 private:
     QWidget* window;
+    QSystemTrayIcon trayIcon;
+    QMenu trayMenu;
 
 public:
     explicit PlatformUtils(QWidget *parent = 0);
@@ -16,6 +20,11 @@ public:
 signals:
 
 public slots:
+    void showAndRaise();
+    void trayActivated(QSystemTrayIcon::ActivationReason reason);
+    void messageClicked();
+    void menuTriggered(QAction* action);
+
     void windowsExtendFrameIntoClientArea(int left, int top, int right, int bottom);
     bool windowsIsCompositionEnabled();
     QColor windowsRealColorizationColor();
